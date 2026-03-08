b60b90d93fab4207a67435e9c27ba00c197439ac
Filipe Chagas <93162962+filipechgs@users.noreply.github.com>
2026-03-07 21:47:20 -0300

    Enable Swagger UI in all environments

--- Program.cs
+++ Program.cs
@@ -53,11 +53,8 @@ builder.Services.AddCors(o =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors(ClientCors);
